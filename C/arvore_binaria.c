e21ca1d6a584752a172d384d25ba16e9af0b841c
marcosfelipp <mfelipe11@yahoo.com.br>
2020-07-08 14:24:47 -0300

    Adicao arvore binaria

--- C/arvore_binaria.c
+++ C/arvore_binaria.c
@@ -3,7 +3,6 @@
 
 typedef struct no {
 	int chave;
-	struct no *pai;
 	struct no *esquerda;
 	struct no *direita;
 } celula;
@@ -14,30 +13,61 @@ typedef celula *arvore;
 arvore constroi_arvore(int chave){
 	arvore r;
 	r = (arvore) malloc(sizeof(celula));
-	r->pai = NULL;
 	r->esquerda = NULL;
 	r->direita = NULL;
 	r->chave = chave;
 	return r;
 }
 
+
 void cria_filho(arvore r, int x, int esq_dir){
 	arvore f;
 	f = (arvore) malloc(sizeof(celula));
-	f->pai = r;
 	f->chave = x;
 	f->esquerda = NULL;
 	f->direita = NULL;
-	// Esquerda -> 1, direita -> 0
-	if(esq_dir == 1) r->esquerda = f;
+	// Esquerda -> 0, direita -> 1
+	if(esq_dir == 0) r->esquerda = f;
 	else r->direita = f;
 
 }
 
+arvore busca_no(arvore r, int x){
+	if(r->chave == x) return r;
+
+	if(r->esquerda != NULL){
+		arvore e = busca_no(r->esquerda, x);
+		if(e != NULL) return e; 
+	}
+	if(r->direita != NULL){
+		arvore r = busca_no(r->direita, x);
+		if(r != NULL) return r;
+	}
+	return NULL;
+}
+
+
+int insere_no_arvore(arvore r, int x, int pai_para_inserir, int esq_dir){
+	arvore arvore_inserir = busca_no(r, pai_para_inserir);
+	if(arvore_inserir != NULL){
+		cria_filho(arvore_inserir, x, esq_dir);
+		return 1;	
+	}else{
+		return 0;	
+	}
+}
+
+int tamanho(arvore r){
+	if(r==NULL) return 0;
+	else return tamanho(r->esquerda) + 1 +tamanho(r->direita);
+}
 
 int main(){
 	arvore r = constroi_arvore(1);
-	cria_filho(r, 2, 1);
-	printf("Hello");
-
+	insere_no_arvore(r, 2, 1, 0);
+	insere_no_arvore(r, 3, 1, 1);
+	insere_no_arvore(r, 4, 2, 0);
+	insere_no_arvore(r, 5, 2, 1);
+	// insere_no_arvore(r, 6, 3, 0);
+	// insere_no_arvore(r, 7, 3, 1);
 }
